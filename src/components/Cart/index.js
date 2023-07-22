b77a092242958b687bbae7d9957e8e3f66c3cebd
ashushikhare9394 <shikhareashwini9394@gmail.com>
2023-07-22 15:38:58 +0000

    solution

---
+++ src/components/Cart/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here'
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+        className="nav-bar-img"
+      />
+    </div>
+  </>
+)
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="nav-bar-img"
+      />
+    </div>
+  </>
+)
+export default Products
